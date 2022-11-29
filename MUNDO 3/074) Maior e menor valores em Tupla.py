f6b0130ba731e6b6ef90d0ff050b6d6041729e36
BLACK™ <107000099+VILHALVA@users.noreply.github.com>
2022-11-28 22:36:26 -0500

    Delete MUNDO 3 directory

--- MUNDO 3/074) Maior e menor valores em Tupla.py
+++
@@ -1,7 +0,0 @@
-#==============MAIOR E MENOR NÚMEROS EM TUPLA:================================
-from random import randint
-números = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
-print("⭐Eu sortei os valores:",end=" ")
-for n in números:
-    print(f"{n}",end=" ")
-print(f"\n⭐O maior valor foi {max(números)}!\n⭐O menor valor foi {min(números)}!")
--- MUNDO 3/097) Um print especial.py
+++
@@ -1,8 +0,0 @@
-#============UM PRINT ESPECIAL:======================================
-def escreva(msg):
-    tam = len(msg)+4
-    print("=" *tam, "\n", msg, "\n", "=" *tam)
-    
-escreva("SAMUEL MARTINS")
-escreva("oi")
-escreva("COMPUTADOR")
