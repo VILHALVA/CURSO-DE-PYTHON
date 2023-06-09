5a7d43d539b80cf7cb306da595d942064102ba28
CODIGOS™ <107000099+VILHALVA@users.noreply.github.com>
2023-06-09 10:47:50 -0500

    Delete MESTRE DO PYTHON directory

--- MESTRE DO PYTHON/01) VARIAVEIS SIMPLES/03) CALCULO DA MEDIA ARITMETICA.py
+++
@@ -1,6 +0,0 @@
-nota1 = float(input("😎Digite a sua 1° Nota da prova:\n>>>"))
-nota2 = float(input("😎Digite a sua 2° Nota da prova:\n>>>"))
-nota3 = float(input("😎Digite a sua 3° Nota da prova:\n>>>"))
-nota4 = float(input("😎Digite a sua 4° Nota da prova:\n>>>"))      
-média = (nota1 + nota2 + nota3 + nota4) / 4    
-print(f"⚡Sua 1° Nota é: ({nota1:.1f});\n⚡Sua 2° Nota é: ({nota2:.1f});\n⚡Sua 3° Nota é: ({nota3:.1f});\n⚡Sua 4° Nota é: ({nota4:.1f});\n⭐A sua MÉDIA é: ({média:.1f})!")
